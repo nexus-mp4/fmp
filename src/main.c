7d48567b2be266335763238b76dff84baf8c51b3
nexus-mp4 <ghilesou3@gmail.com>
2026-08-25 17:41:17 +0100

    added more keybinds, and a help menu

--- src/main.c
+++ src/main.c
@@ -13,36 +13,42 @@
 #define RESET "\x1b[0m"
 #define EFFECT_BG "\x1b[37;40m"
 
+void spawnbox() {
+    WINDOW *win = newwin(9, 30, 13, 57); 
+    
+    wborder(win, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE,
+            ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+    
+    mvwprintw(win, 1, 1, " 'K': decrease speed");
+    mvwprintw(win, 2, 1, " 'L': increase speed");
+    mvwprintw(win, 3, 1, " '[': seek 5");
+    mvwprintw(win, 4, 1, " ']': seek -5");
+    mvwprintw(win, 5, 1, " '>': next");
+    mvwprintw(win, 6, 1, " '<': previous");
+    
+    wgetch(win);
+    werase(win);
+    wrefresh(win);
+    delwin(win);
+}
 
-char** get_audio_metadata(const char* filepath) {
-    AVFormatContext* ctx = NULL;
-    char** result = malloc(3 * sizeof(char*));
-    char* filename = strrchr(filepath, '/');
-    if (!filename) filename = strrchr(filepath, '\\');
-    result[0] = strdup(filename ? filename + 1 : filepath);
-    result[1] = result[2] = NULL;
-    if (avformat_open_input(&ctx, filepath, NULL, NULL) == 0) {
+char** get_audio_metadata(const char* filepath) {                               
+    AVFormatContext* ctx = NULL;                                                
+    char** result = malloc(3 * sizeof(char*));                                  
+    char* filename = strrchr(filepath, '/');                                    
+    if (!filename) filename = strrchr(filepath, '\\');                          
+    result[0] = strdup(filename ? filename + 1 : filepath);                     
+    result[1] = result[2] = NULL;                                               
+    if (avformat_open_input(&ctx, filepath, NULL, NULL) == 0) {                 
         AVDictionaryEntry* title = av_dict_get(ctx->metadata, "title", NULL, 0);
         AVDictionaryEntry* artist = av_dict_get(ctx->metadata, "artist", NULL, 0);
-        if (title && artist) {
-            result[1] = strdup(title->value);
-            result[2] = strdup(artist->value);
-        }
-        avformat_close_input(&ctx);
-    }
-    return result;
+        if (title) result[1] = strdup(title->value);                                   
+        if (artist) result[2] = strdup(artist->value);                                  
+        avformat_close_input(&ctx);                                             
+    }                                                                           
+    return result;                                                              
 }
 
-void displayqueue(char *path) {
-    DIR *dir = opendir(path);
-    struct dirent *entry;
-    if ((entry = readdir(dir)) != NULL) {
-        printw ( EFFECT "queue:" RESET);
-    }
-    while ((entry = readdir(dir)) != NULL) {
-        printw("%s\n", entry->d_name);
-    }
-}
 
 void check_file(char *file) {
     FILE *fp = fopen(file, "r");
@@ -52,14 +58,14 @@ void check_file(char *file) {
     }   
 }
 
-char *cmdgen(char *path) {
-    char *ret;
+int cmdgen(char *path) {
+    int ret;
     DIR *dp = opendir(path);
     FILE *fp = fopen(path, "r");
     if (dp != NULL) {
-        ret = "dir";
+        ret = 0;
     } else if (fp != NULL) {
-        ret = "file";
+        ret = 1;
     }
     return ret;
 }
@@ -68,7 +74,9 @@ void curseyou(char *file, mpv_handle *fmp) {
     WINDOW *win = initscr();
     cbreak();        
     noecho();
+    curs_set(0);
     int key;
+
     char **data = get_audio_metadata(file);
     if (data[1] && data[2]) {
         mvprintw(0, 2, "%s - %s ", data[1], data[2]);
@@ -79,7 +87,7 @@ void curseyou(char *file, mpv_handle *fmp) {
     int max_y, max_x;
     getmaxyx(win, max_y, max_x);
     double speed = 1.0;
-    mvprintw(max_y - 1, 0, "'Q': quit | SPACEBAR: pause/play | '[': seek -5 | ']': seek 5 | 'k': decrease speed | 'l': increase speed");
+    mvprintw(max_y - 1, 0, "'Q': quit | SPACEBAR: pause/play | 'H': keybinds");
     while ((key = getch()) != 'q') {
         switch(key) {
             case ']':
@@ -106,6 +114,18 @@ void curseyou(char *file, mpv_handle *fmp) {
                 mpv_command_string(fmp, "cycle pause");
                 break;
             }
+            case 'h': {
+                spawnbox(); 
+                break;
+            }
+            case '.': {
+                const char *cmd[] = {"playlist-next", NULL};
+                mpv_command(fmp, cmd);
+            }
+            case ',': {
+               const char *cmd[] = {"playlist-previous", NULL};
+               mpv_command(fmp, cmd);
+            }
         }
     wrefresh(win); refresh();
     }
@@ -117,7 +137,6 @@ void curseyou(char *file, mpv_handle *fmp) {
 int play(char *file) {
     mpv_handle *fmp = mpv_create();
     mpv_initialize(fmp);
-    char *c = cmdgen(file);
     const char *command[] = {"loadfile", file, NULL};
     mpv_set_option_string(fmp, "video", "no");
     mpv_command(fmp, command);
@@ -128,10 +147,5 @@ int play(char *file) {
 
 int main(int argc, char **argv) {    
     check_file(argv[1]);
-    if (cmdgen(argv[1]) == "file") {
-        play(argv[1]);
-    } else if (cmdgen(argv[1]) == "dir") {
-        play(argv[1]);
-        displayqueue(argv[1]);
-    }
+    play(argv[1]);
 }
